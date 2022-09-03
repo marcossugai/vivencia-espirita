aef0c4b02d8efd481f3663413bd2b2a13f66f9d6
Marcos Sugai Ogama <pazzino2000@yahoo.it>
2022-09-03 19:04:39 -0300

    Iniciando

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './styles/reset.css'
+import './styles/settings/colors.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
